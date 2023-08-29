1fec4276e922e6b7808c7d675f1bf2298c0357b1
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-08-29 16:45:51 +0200

    Renamed Initialize-SepmConfiguration to load & run the function at the end of the module

--- Source/Public/zz_Initialize-SepmConfiguration.ps1
+++ Source/Public/zz_Initialize-SepmConfiguration.ps1
@@ -1,6 +1,17 @@
+####################################
+# Init script for the whole module #
+####################################
+
+## This is the initialization script for the module.  It is invoked at the end of the module's
+## prefix file as "zz_" to load this module at last.  This is done to ensure that all other functions are first loaded
+
 # The credentials used to authenticate to the SEPM server.
 [PSCredential] $script:Credential = $null
 [PSCredential] $script:accessToken = $null
+
+# SEPM Server configuration
+[string] $script:ServerAddress = $null
+[string] $script:BaseURL = $null
 
 # The location of the file that we'll store any settings that can/should roam with the user.
 [string] $script:configurationFilePath = [System.IO.Path]::Combine(
@@ -27,6 +38,8 @@ $script:seenTokenWarningThisSession = $false
 # The session-cached copy of the module's configuration properties
 [PSCustomObject] $script:configuration = $null
 
+
+
 function Initialize-SepmConfiguration {
     <#
     .SYNOPSIS
@@ -47,3 +60,6 @@ function Initialize-SepmConfiguration {
     $script:configuration = Import-SepmConfiguration -Path $script:configurationFilePath
     
 }
+
+# Invoke the initialization method to populate the configuration
+Initialize-SepmConfiguration
