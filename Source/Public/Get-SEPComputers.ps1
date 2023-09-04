42c2ff17125d48e4b38fa343c9ee458233cddde8
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-09-04 11:02:47 +0200

    Fixed querry using correct querystrings with GET method

--- Source/Public/Get-SEPComputers.ps1
+++ Source/Public/Get-SEPComputers.ps1
@@ -1,119 +1,109 @@
-Function Get-SEPComputers {
+function Get-SEPComputers {
     <#
     .SYNOPSIS
-    Displays a short or specific list of computers and their information from the Symantec Database
- 
-    .PARAMETER ComputerName
-    Specifies the computer to return information on from the Symantec Database
- 
+        Gets the information about the computers in a specified domain
+    .DESCRIPTION
+        Gets the information about the computers in a specified domain. A system administrator account is required for this REST API.
     .EXAMPLE
-    Get-SEPComputers -ComputerName TESTPC OR
-    Get-SEPComputers
-    #>
+        Get-SEPComputers
+
+        Gets computer details for all computers in the domain
+    .EXAMPLE
+        Get-SEPComputers -ComputerName "ComputerName"
+
+        Gets computer details for the specified computer ComputerName
+    .EXAMPLE
+        "MyComputer" | Get-SEPComputers
+
+        Gets computer details for the specified computer MyComputer
+#>
     [CmdletBinding()]
     Param (
-        [Parameter()][ValidateNotNullOrEmpty()][String]$ComputerName
+        # ComputerName
+        [Parameter(
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias("Hostname", "DeviceName", "Device", "Computer", "ComputerName")]
+        [String]
+        $ComputerName
     )
-    if ($PSVersionTable.PSVersion.Major -lt 6) {
-        if ($null -ne $headers) {
-            if ($null -ne $ComputerName) {
-                $URI = $BaseURL + "/computers?computerName=$ComputerName"
-                try {
-                    (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                } catch {
-                    "An error was found with this command. Please review the resultant error for details."
-                    $RESTError = Get-RestError($_)
-                    "Errors: $RESTError"
-                }
-            } else {
-                $URI = $BaseURL + '/computers'
-                try {
-                    (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                } catch {
-                    "An error was found with this command. Please review the resultant error for details."
-                    $RESTError = Get-RestError($_)
-                    "Errors: $RESTError"
-                }
-            }
+
+    begin {
+        # initialize the configuration
+        $test_token = Test-SEPMAccessToken
+        if ($test_token -eq $false) {
+            Get-SEPMAccessToken
         }
-        if ($null -eq $headers) {
-            Get-SEPToken
-            if ($null -ne $headers) {
-                if ($null -ne $ComputerName) {
-                    $URI = $BaseURL + "/computers?computerName=$ComputerName"
-                    try {
-                        (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                    } catch {
-                        "An error was found with this command. Please review the resultant error for details."
-                        $RESTError = Get-RestError($_)
-                        "Errors: $RESTError"
-                    }
-                } else {
-                    $URI = $BaseURL + '/computers'
-                    try {
-                        (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                    } catch {
-                        "An error was found with this command. Please review the resultant error for details."
-                        $RESTError = Get-RestError($_)
-                        "Errors: $RESTError"
-                    }
-                }
-            }
+        $URI = $script:BaseURL + "/computers"
+        $headers = @{
+            "Authorization" = "Bearer " + $script:accessToken.token
+            "Content"       = 'application/json'
         }
     }
-    if ($PSVersionTable.PSVersion.Major -ge 6) {
-        if ($null -ne $headers) {
-            if ($null -ne $ComputerName) {
-                $URI = $BaseURL + "/computers?computerName=$ComputerName"
-                try {
-                    if ($Global:SkipCert -eq $true) {
-                        (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers -SkipCertificateCheck).content
-                    } else {
-                        (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                    }
-                } catch {
-                    Get-RestErrorDetails
-                }
-            } else {
-                $URI = $BaseURL + '/computers'
-                try {
-                    if ($Global:SkipCert -eq $true) {
-                        (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers -SkipCertificateCheck).content
-                    } else {
-                        (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                    }
-                } catch {
-                    Get-RestErrorDetails
-                }
-            }
+
+    process {
+        $allResults = @()
+
+        if (-not $ComputerName) {
+            $ComputerName = ""
         }
-        if ($null -eq $headers) {
-            Get-SEPToken
-            if ($null -ne $headers) {
-                if ($null -ne $ComputerName) {
-                    $URI = $BaseURL + "/computers?computerName=$ComputerName"
-                    try {
-                        if ($Global:SkipCert -eq $true) {
-                            (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers -SkipCertificateCheck).content
-                        } else {
-                            (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                        }
-                    } catch {
-                        Get-RestErrorDetails
+
+        $QueryStrings = @{
+            sort         = "COMPUTER_NAME"
+            pageIndex    = 1
+            pageSize     = 100
+            computerName = $ComputerName
+        }
+
+        # Contruct the URI
+        $builder = New-Object System.UriBuilder($URI)
+        $query = [System.Web.HttpUtility]::ParseQueryString($builder.Query)
+        foreach ($param in $QueryStrings.GetEnumerator()) {
+            $query[$param.Key] = $param.Value
+        }
+        $builder.Query = $query.ToString()
+        $URI = $builder.ToString()
+    
+        # Invoke the request
+        # If the version of PowerShell is 6 or greater, then we can use the -SkipCertificateCheck parameter
+        # else we need to use the Skip-Cert function if self-signed certs are being used.
+        do {
+            try {
+                # Invoke the request params
+                $params = @{
+                    Method  = 'GET'
+                    Uri     = $URI
+                    headers = $headers
+                }
+                if ($script:accessToken.skipCert -eq $true) {
+                    if ($PSVersionTable.PSVersion.Major -lt 6) {
+                        Skip-Cert
+                        $resp = Invoke-RestMethod @params
+                    } else {
+                        $resp = Invoke-RestMethod @params -SkipCertificateCheck
                     }
                 } else {
-                    $URI = $BaseURL + '/computers'
-                    try {
-                        if ($Global:SkipCert -eq $true) {
-                            (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers -SkipCertificateCheck).content
-                        } else {
-                            (Invoke-RestMethod -Method GET -Uri $URI -Headers $headers).content
-                        }
-                    } catch {
-                        Get-RestErrorDetails
-                    }
+                    $resp = Invoke-RestMethod @params
+                } 
+                
+                # Process the response
+                $allResults += $resp.content
+
+                # Increment the page index & update URI
+                $QueryStrings.pageIndex++
+                $query = [System.Web.HttpUtility]::ParseQueryString($builder.Query)
+                foreach ($param in $QueryStrings.GetEnumerator()) {
+                    $query[$param.Key] = $param.Value
                 }
+                $builder.Query = $query.ToString()
+                $URI = $builder.ToString()
+            } catch {
+                Write-Warning -Message "Error: $_"
             }
-        }
+        } until ($resp.lastPage -eq $true)
+
+        # return the response
+        return $allResults
     }
 }
