b36ff15bd9abfe31045f13683e22c5588cc8273e
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-09-04 11:03:23 +0200

    Fixed GET method with query builder

--- Source/Public/Get-SEPAdmins.ps1
+++ Source/Public/Get-SEPAdmins.ps1
@@ -1,78 +1,103 @@
 Function Get-SEPAdmins {
     <#
-.SYNOPSIS
-Displays a list of admins in the Symantec Database
-.EXAMPLE
-Get-SEPAdmins
- 
-.PARAMETER AdminName
-Displays only a specific user from the Admin List
-Get-SEPAdmins -AdminName admin
- 
-.EXAMPLE
-Get-SEPAdmins
- 
-.NOTES
-General notes
+    .SYNOPSIS
+        Displays a list of admins in the Symantec Database
+
+    .DESCRIPTION
+        Gets the list of administrators for a particular domain.
+
+        The Git repo for this module can be found here: https://github.com/Douda/PSSymantecSEPM
+
+    .PARAMETER AdminName
+        Displays only a specific user from the Admin List
+
+    .EXAMPLE
+    Get-SEPAdmins -AdminName admin
+
+    .EXAMPLE
+    Get-SEPAdmins
 #>
     [CmdletBinding()]
     Param (
         # AdminName
-        [Parameter()]
+        [Parameter(
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
         [String]
+        [Alias("Admin", "AdminName")]
         $AdminName
     )
 
-    # initialize the configuration
-    $test_token = Test-SEPMAccessToken
-    if ($test_token -eq $false) {
-        Get-SEPMAccessToken
-    }
-    $URI = $script:BaseURL + "/admin-users"
-    $headers = @{
-        "Authorization" = "Bearer " + $script:accessToken.token
-        "Content"       = 'application/json'
-    }
-    $params = @{
-        Method  = 'GET'
-        Uri     = $URI
-        headers = $headers
+    begin {
+        # initialize the configuration
+        $test_token = Test-SEPMAccessToken
+        if ($test_token -eq $false) {
+            Get-SEPMAccessToken
+        }
+        $URI = $script:BaseURL + "/admin-users"
+        $headers = @{
+            "Authorization" = "Bearer " + $script:accessToken.token
+            "Content"       = 'application/json'
+        }
     }
 
-    # Invoke the request
-    # If the version of PowerShell is 6 or greater, then we can use the -SkipCertificateCheck parameter
-    # else we need to use the Skip-Cert function if self-signed certs are being used.
-    switch ($PSVersionTable.PSVersion.Major) {
-        { $_ -ge 6 } { 
-            try {
-                # if ($script:SkipCert -eq $true) {
-                if ($script:accessToken.skipCert -eq $true) {
-                    $resp = Invoke-RestMethod @params -SkipCertificateCheck
-                } else {
-                    $resp = Invoke-RestMethod @params
+    process {
+        # URI query strings
+        $QueryStrings = @{
+            domain = $script:configuration.domain
+        }
+
+        # Contruct the URI
+        $builder = New-Object System.UriBuilder($URI)
+        $query = [System.Web.HttpUtility]::ParseQueryString($builder.Query)
+        foreach ($param in $QueryStrings.GetEnumerator()) {
+            $query[$param.Key] = $param.Value
+        }
+        $builder.Query = $query.ToString()
+        $URI = $builder.ToString()
+
+        # Invoke the request
+        # If the version of PowerShell is 6 or greater, then we can use the -SkipCertificateCheck parameter
+        # else we need to use the Skip-Cert function if self-signed certs are being used.
+        switch ($PSVersionTable.PSVersion.Major) {
+            { $_ -ge 6 } { 
+                try {
+                    $params = @{
+                        Method  = 'GET'
+                        Uri     = $URI
+                        headers = $headers
+                    }
+                    if ($script:accessToken.skipCert -eq $true) {
+                        $resp = Invoke-RestMethod @params -SkipCertificateCheck
+                    } else {
+                        $resp = Invoke-RestMethod @params
+                    }
+                } catch {
+                    Write-Warning -Message "Error: $_"
                 }
-            } catch {
-                "An error was found with this command. Please review the resultant error for details."
-                $RESTError = Get-RestError($_)
-                "Errors: $RESTError"
             }
-        }
-        default {
-            # if ($script:SkipCert -eq $true) {
-            if ($script:accessToken.skipCert -eq $true) {
-                Skip-Cert
-                $resp = Invoke-RestMethod @params
-            } else {
-                $resp = Invoke-RestMethod @params
-            }
-        }
-    }
+            default {
+                try {
+                    if ($script:accessToken.skipCert -eq $true) {
+                        Skip-Cert
+                        $resp = Invoke-RestMethod @params
+                    } else {
+                        $resp = Invoke-RestMethod @params
+                    }
+                } catch {
+                    Write-Warning -Message "Error: $_"
+                }
 
-    # Process the response
-    if ([string]::IsNullOrEmpty($AdminName)) {
-        return $resp
-    } else {
-        $resp = $resp | Where-Object { $_.loginName -eq $AdminName }
-        return $resp
+            }
+        }
+
+        # Process the response
+        if ([string]::IsNullOrEmpty($AdminName)) {
+            return $resp
+        } else {
+            $resp = $resp | Where-Object { $_.loginName -eq $AdminName }
+            return $resp
+        }
     }
 }
