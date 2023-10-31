03b530fbcc5880a7bc19ed0fe65aa0b308a5277d
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-10-31 19:18:18 +0100

    added Infected Status CmdLet

---
+++ Source/Public/Get-SEPClientInfectedStatus.ps1
@@ -0,0 +1,42 @@
+function Get-SEPClientInfectedStatus {
+    <# 
+    .SYNOPSIS
+        Gets SEP Clients with Infected or Clean status
+    .DESCRIPTION
+        Gets SEP Clients with Infected or Clean status
+        NOTES : Clean status is just Infected = 0
+
+    .INPUTS
+        None
+    .OUTPUTS
+        List of SEP Clients with Infected status
+    .PARAMETER
+        Clean
+        If specified, returns SEP Clients with Clean status
+    .EXAMPLE
+        Get-SEPClientInfectedStatus
+
+        Gets computer details for all computers in the domain
+    .EXAMPLE
+        Get-SEPClientInfectedStatus -Clean
+
+        Gets computer details for all computers in the domain that are not infected
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [switch]
+        $Clean
+    )
+
+    process {
+        if ($clean) {
+            $non_infected = Get-SEPComputers | Where-Object { $_.infected -ne 1 }
+            return $non_infected
+        } else {
+            $infected = Get-SEPComputers | Where-Object { $_.infected -eq 1 }
+            return $Infected
+        }
+    }
+}
