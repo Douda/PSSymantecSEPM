b784bf48469c29ba21433a9ac5bc7201a2f7ab6e
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-12-04 18:29:55 +0100

    renamed variables

--- Tests/Common-Init.ps1
+++ Tests/Common-Init.ps1
@@ -4,14 +4,14 @@ function Initialize-CommonInitSetup {
     # init
     $script:moduleName = 'PSSymantecSEPM'
     $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
-    $BuildModuleSourcePath = Join-Path -Path $moduleRootPath -ChildPath "Source\PSSymantecSEPM.psd1"
-    $importModuleFilePath = Join-Path -Path $moduleRootPath -ChildPath "Output\PSSymantecSEPM\0.0.1\PSSymantecSEPM.psm1"
+    $Build = Join-Path -Path $moduleRootPath -ChildPath "Source\PSSymantecSEPM.psd1"
+    $import = Join-Path -Path $moduleRootPath -ChildPath "Output\PSSymantecSEPM\0.0.1\PSSymantecSEPM.psm1"
     
-    # If $importModuleFilePath is not found, build the module
-    if (!(Test-Path -Path $importModuleFilePath)) {
-        Build-Module -SourcePath $BuildModuleSourcePath -SemVer 0.0.1
+    # If module isn't present on the disk, build it with hardcoded version 0.0.1
+    if (!(Test-Path -Path $import)) {
+        Build-Module -SourcePath $Build -SemVer 0.0.1
     }
-    Import-Module -Name "$importModuleFilePath" -Force
+    Import-Module -Name "$import" -Force
     Write-Host "Module $script:moduleName loaded"
 }
 
