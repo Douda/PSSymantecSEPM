500447d5a327672464d76c89adb4320dbc3f0d1f
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2023-12-05 12:38:21 +0100

    Forces Build-module to run to reflect current code changes

--- Tests/Common-Init.ps1
+++ Tests/Common-Init.ps1
@@ -8,9 +8,11 @@ function Initialize-CommonInitSetup {
     $import = Join-Path -Path $moduleRootPath -ChildPath "Output\PSSymantecSEPM\0.0.1\PSSymantecSEPM.psm1"
     
     # If module isn't present on the disk, build it with hardcoded version 0.0.1
-    if (!(Test-Path -Path $import)) {
-        Build-Module -SourcePath $Build -SemVer 0.0.1
-    }
+    # if (!(Test-Path -Path $import)) {
+    # TODO add Test-Path condition once the code is ready for release
+    Build-Module -SourcePath $Build -SemVer 0.0.1
+    # }
+    
     Import-Module -Name "$import" -Force
     # Write-Host "Module $script:moduleName loaded"
 }
