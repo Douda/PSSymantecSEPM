102de136b7c591ca5fecc1dc09ab29879ea93a25
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2024-02-08 15:27:48 +0100

    Removed build-module versioning for development

--- __For_Dev_And_Maintenance/Build_and_load_module.ps1
+++ __For_Dev_And_Maintenance/Build_and_load_module.ps1
@@ -1,10 +1,11 @@
 # Setting up Paths & Variables
 $ModuleDevPath = Split-Path -Path $PSScriptRoot -Parent
 $BuildModuleSourcePath = $ModuleDevPath + "\Source\PSSymantecSEPM.psd1"
-$MajorMinorPatch = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand MajorMinorPatch
+# $MajorMinorPatch = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand MajorMinorPatch
 $FullSemVer = dotnet-gitversion | ConvertFrom-Json | Select-Object -Expand FullSemVer
-$ImportModulePath = $ModuleDevPath + "\Output\PSSymantecSEPM\" + $MajorMinorPatch + "\PSSymantecSEPM.psm1"
+# $ImportModulePath = $ModuleDevPath + "\Output\PSSymantecSEPM\" + $MajorMinorPatch + "\PSSymantecSEPM.psm1"
+$ImportModulePath = $ModuleDevPath + "\Output\PSSymantecSEPM\PSSymantecSEPM.psm1"
 
 # Build Module
-Build-Module -SourcePath $BuildModuleSourcePath -SemVer $FullSemVer
+Build-Module -SourcePath $BuildModuleSourcePath -SemVer $FullSemVer -UnversionedOutputDirectory
 Import-Module -Name "$ImportModulePath" -Force
