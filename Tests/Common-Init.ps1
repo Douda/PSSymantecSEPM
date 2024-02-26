a3a21dad0b25dd5bf735335e35ac65e6905df2b6
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2024-02-26 18:39:37 +0100

    Fix import path in Common-Init.ps1

--- Tests/Common-Init.ps1
+++ Tests/Common-Init.ps1
@@ -5,7 +5,7 @@ function Initialize-CommonInitSetup {
     $script:moduleName = 'PSSymantecSEPM'
     $moduleRootPath = Split-Path -Path $PSScriptRoot -Parent
     $Build = Join-Path -Path $moduleRootPath -ChildPath "Source\PSSymantecSEPM.psd1"
-    $import = Join-Path -Path $moduleRootPath -ChildPath "Output\PSSymantecSEPM\0.0.1\PSSymantecSEPM.psm1"
+    $import = Join-Path -Path $moduleRootPath -ChildPath "Output\PSSymantecSEPM\PSSymantecSEPM.psm1"
     
     # If module isn't present on the disk, build it with hardcoded version 0.0.1
     # if (!(Test-Path -Path $import)) {
