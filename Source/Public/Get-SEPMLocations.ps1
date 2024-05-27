d5c5cb6551738ffc8380b87c1cda2d07e42983b0
Aurélien BOUMANNE <aurelien.boumanne@gmail.com>
2024-05-27 12:01:01 +0200

    Updated help

--- Source/Public/Get-SEPMLocations.ps1
+++ Source/Public/Get-SEPMLocations.ps1
@@ -19,11 +19,14 @@ function Get-SEPMLocations {
 
         Gets the list of location names and their IDs for the specified group
     .EXAMPLE
-        PS C:\PSSymantecSEPM> Get-SEPMGroups | Select-Object -ExpandProperty id | Get-SEPMLocations
+        PS C:\PSSymantecSEPM> Get-SEPMGroups | Select-Object -ExpandProperty id | Get-SEPMLocations | ft
 
-        name                  id
-        ----                  --
-        Default               33CE4894AC1485D12E3AAC763CF9A71B
+        locationName          locationId                       groupName                      groupId                          groupFullPathName
+        ------------          ----------                       ---------                      -------                          -----------------
+        Default               60B5C584AC17D44C6CC60471B7292FC4 My Company                     BDDDF2E6AC17D44C7007E1EA7851E110 My Company
+        Default               60B5C584AC17D44C6CC60471B7292FC4 Default Group                  EA26D2D9AC17D44C532C651C62105B61 My Company\Default Group
+        Default               60B5C584AC17D44C6CC60471B7292FC4 group 1                        10314F0DAC1485D157E3089CEB47D5BC My Company\group 1
+        Default               60B5C584AC17D44C6CC60471B7292FC4 sub group 1                    6CB7099BAC1485D118EA465F45C4AE54 My Company\group 1\sub group 1
 
         Gets the list of location names and their IDs for all groups from the list of groups via pipeline
 #>
