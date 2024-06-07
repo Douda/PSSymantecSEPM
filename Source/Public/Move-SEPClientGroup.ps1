fa45076e6da0c925e9dc51cd035759e76ce6944b
Aurelien BOUMANNE <aurelien.boumanne@gmail.com>
2024-06-07 14:51:51 +0200

    refactor: - Add mandatory parameter for GroupName in Move-SEPClientGroup.ps1 - Updated response with PSType SEPM.MoveClientGroupResponse - Updated response with additional fields to identify SEP client moved & target group

--- Source/Public/Move-SEPClientGroup.ps1
+++ Source/Public/Move-SEPClientGroup.ps1
@@ -33,6 +33,7 @@ function Move-SEPClientGroup {
 
         # ComputerName
         [Parameter(
+            Mandatory = $true,
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true
         )]
@@ -42,7 +43,7 @@ function Move-SEPClientGroup {
 
         # group name
         [Parameter(
-            ValueFromPipelineByPropertyName = $true
+            Mandatory = $true
         )]
         [Alias("Group")]
         [String]
@@ -111,7 +112,18 @@ function Move-SEPClientGroup {
             Write-Warning -Message "Error: $_"
         }
 
+        $fullResponse = [PSCustomObject]@{
+            computerName        = $ComputerName
+            computerHardwareKey = $hardwareKey
+            targetGroup         = $GroupName
+            responseCode        = $resp.responseCode
+            responseMessage     = $resp.responseMessage
+        }
+
+        # Add a PSTypeName to the object
+        $fullResponse.PSObject.TypeNames.Insert(0, 'SEPM.MoveClientGroupResponse')
+
         # return the response
-        return $resp
+        return $fullResponse
     }
 }
